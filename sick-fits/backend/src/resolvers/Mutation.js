ee349b1ae030452b32cb01a02c2139403aed676f
Vishal Sharma <vish.sharma0203@gmail.com>
2018-10-25 22:30:47 +0530

    added queries to create and get all items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
-const Mutation = {};
+const Mutation = {
+    async createItem(parent, args, ctx, info) {
+        return await ctx.db.mutation.createItem(args);
+    }
+};
 
 module.exports = Mutation;
