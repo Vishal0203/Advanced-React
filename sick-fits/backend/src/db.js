791fbb852d27d0952139a962dc098799b7537d83
Vishal Sharma <vish.sharma0203@gmail.com>
2018-10-25 18:07:34 +0530

    graphql progess so far

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,10 @@
+// connects to the prisma db
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    debug: false
+});
+
+module.exports = db;
--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// Todo use express middleware to handle cookies (JWT)
+// Todo use express middleware to populate current user
+
+const options = {
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+};
+
+server.start(options, (server) => {
+    console.log(`Server running on http://localhost:${server.port}`)
+});
+
